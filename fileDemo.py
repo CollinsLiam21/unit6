f7d79eee3c02c1143fcb1dbaeadc8b7caf05ce34
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-05-09 11:10:51 -0400

    Updated from Brython Server: 5/9/2018 11:10:51 AM

--- fileDemo.py
+++ fileDemo.py
@@ -1,1 +1,13 @@
+#Liam Collins
+#5/9/18
+#fileDemo.py
 
+file = open('engmix.txt')
+
+numWords = 0
+for line in file:
+    if 'tso' in line:
+        print(line.strip())
+    numWords += 1
+
+print(numWords)
