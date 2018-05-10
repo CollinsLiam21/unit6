2c09e05d1173fe2c88b405147a4a30da65e9683d
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-05-10 08:17:13 -0400

    Updated from Brython Server: 5/10/2018 8:17:12 AM

--- longestDictionaryWord.py
+++ longestDictionaryWord.py
@@ -1,1 +1,11 @@
+#Liam Collins
+#5/10/18
+#longestDictionaryWord.py
 
+file = open('engmix.txt')
+
+longest = 0
+for item in file:
+    if len(item) > longest:
+        longest = item
+print(longest)
