653ef06d6ff492b7a50ac3ab1c5afed0c7a01ad4
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-05-10 08:38:38 -0400

    Updated from Brython Server: 5/10/2018 8:38:38 AM

--- howManyWords.py
+++ howManyWords.py
@@ -1,1 +1,13 @@
+#Liam Collins
+#5/10/18
+#howManyWords.py
 
+file = open('engmix.txt')
+
+L = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+for item in file:
+    L[len(item)-1] = L[len(item)-1] + 1
+
+i = 1
+while i<=21:
+    print(i,
