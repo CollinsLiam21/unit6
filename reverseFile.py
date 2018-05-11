bbb98103d43ee6e454fa1b826029ca2a07810667
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-05-11 08:42:58 -0400

    Updated from Brython Server: 5/11/2018 8:42:57 AM

--- reverseFile.py
+++ reverseFile.py
@@ -6,9 +6,11 @@ file = open(input('Enter file name: '))
 
 new = []
 for line in file:
-    words = line.split()
+    line = line.split()
     if len(line)>0:
-        new.append(words)
+        new.append(line)
 
-print(new)
+new.reverse()
+for word in new:
+    print(word)
 
