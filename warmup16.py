7946cb1a367a7d111526cb752ef9d5e74be4da6a
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-05-11 08:28:48 -0400

    Updated from Brython Server: 5/11/2018 8:28:47 AM

--- warmup16.py
+++ warmup16.py
@@ -1,1 +1,11 @@
+#Liam Collins
+#5/11/18
+#warmup16.py
 
+file = open('engmix.txt')
+
+for words in file:
+    words = words.strip()
+    if len(words) > 0:
+        if words[0] == 'l' and words[-1] == 'c':
+            print(words)
