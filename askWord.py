a24cfe139fe8d865f85d7b914c01e3854a55aa9d
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-05-14 08:34:56 -0400

    Updated from Brython Server: 5/14/2018 8:34:55 AM

--- askWord.py
+++ askWord.py
@@ -1,14 +1,13 @@
 word = input('Enter a word: ')
 file = open('engmix.txt')
-filzz = open('engmix.txt').split()
 
 end = True
-while end:
-    for line in file:
-        if line.strip() == word:
-            print(word, 'is in the dictionary!')
-            end = False
+for line in file:
+    if line.strip() == word:
+        print(word, 'is in the dictionary!')
+        end = False
+        break
 
-        
-        
+if end == True:
+    print(word, 'is not in dictionary')
     
