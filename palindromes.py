356325c44bb496cfbb6ac817177d4e4b9ab39183
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-05-16 08:33:06 -0400

    Updated from Brython Server: 5/16/2018 8:33:05 AM

--- palindromes.py
+++ palindromes.py
@@ -4,7 +4,11 @@
 
 file = open('engmix.txt')
 
+
 for item in file:
     if len(item)%2==0:
-        new.append(item)
-        item[:len(item)//2] == item[len(item)//2:]
+        if item[:len(item)//2] == item[len(item)//2:]:
+            print(item)
+    if len(item)%2 != 0:
+        if item[:len(item)//2] == item[(len(item)//2 + 1):]:
+            print(item)
