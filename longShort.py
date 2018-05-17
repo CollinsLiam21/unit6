8a2073339dc9b99a730ec7385995846d59fcf7ed
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-05-17 08:46:56 -0400

    Updated from Brython Server: 5/17/2018 8:46:55 AM

--- longShort.py
+++ longShort.py
@@ -6,13 +6,14 @@ file = open('engmix.txt')
 
 longList = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 shortList = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+alph = 'abcdefghijklmnopqrstuvwxyz'
 
-for line in item:
+for line in file:
     line = line.strip()
-    if len(line) > len(longList[longList.index(line[0])]:
-        longList[longList.index(line[0])] = line
-    elif len(line) < len(shortList[shortList.index(line[0])]:
-        shortList[shortList.index(line[0])] = line
+    if len(line) > len(longList[alph.index(line[0])]):
+        longList[alph.index(line[0])] = line
+    elif len(line) < len(shortList[alph.index(line[0])]):
+        shortList[alph.index(line[0])] = line
 
 print(longList.strip())
 print(shortList.strip())
