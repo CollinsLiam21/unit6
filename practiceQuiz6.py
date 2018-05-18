886b4fb306c251ecf4a4b1ff6d156853de32afb2
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-05-18 08:32:32 -0400

    Updated from Brython Server: 5/18/2018 8:32:31 AM

--- practiceQuiz6.py
+++ practiceQuiz6.py
@@ -9,3 +9,12 @@ for line in file:
     line = line.strip()
     if line.count('c') == 3 and line.count('p') == 2:
         print(line)
+
+#Program 2
+for line in file:
+    line = line.strip()
+    i = 0
+    if len(line) > 0:
+        if line[0] == 'r':
+            i += 1
+print(i)
