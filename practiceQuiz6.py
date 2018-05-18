0adf66638daebb617b69a772f22f78f85c5da5a2
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-05-18 08:30:06 -0400

    Updated from Brython Server: 5/18/2018 8:30:05 AM

--- practiceQuiz6.py
+++ practiceQuiz6.py
@@ -1,1 +1,11 @@
+#Liam Collins
+#5/18/18
+#practiceQuiz6.py
 
+file = open('engmix.txt')
+
+#Program 1
+for line in file:
+    line = line.strip()
+    if line.count('c') == 3 and line.count('p') == 2:
+        print(line)
