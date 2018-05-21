4b55b1d0c051285b4803b6d122f048b85e49903b
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-05-21 08:06:51 -0400

    Updated from Brython Server: 5/21/2018 8:06:50 AM

--- quiz6.py
+++ quiz6.py
@@ -5,8 +5,15 @@
 file = open('engmix.txt')
 
 #Program 1
-letter = input('Enter a letter: ')
+'''letter = input('Enter a letter: ')
 for line in file:
     line = line.strip()
     if line.count(letter) == 4:
+        print(line)'''
+
+#Program 2
+for line in file:
+    line = line.strip()
+    if line[0] == line[4] and line[4] == line[8]:
         print(line)
+        break
