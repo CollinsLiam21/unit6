2b268dc235a812c5468252c1b629a02026a7e79e
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-05-21 08:03:24 -0400

    Updated from Brython Server: 5/21/2018 8:03:23 AM

--- quiz6.py
+++ quiz6.py
@@ -1,1 +1,11 @@
+#Liam Collins
+#5/21/18
+#quiz6.py
 
+file = open('engmix.txt')
+
+#Program 1
+letter = input('Enter a letter: ')
+for line in file:
+    if line.count(letter) == 4:
+        print(line)
