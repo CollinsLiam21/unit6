18da766ea44fd89bf38b0ea0e11b0164443e74a3
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-05-23 08:39:34 -0400

    Updated from Brython Server: 5/23/2018 8:39:34 AM

--- actualOthello.py
+++ actualOthello.py
@@ -1,1 +1,17 @@
+#Liam Collins
+#5/23/18
+#actualOthello.py
 
+def buildBoard(board):
+    for r in range(0,3):
+        for c in range(0,3):
+            print(board[r][c],end=' ')
+        print()
+
+board = [['a','b','c'],['d','e','f'],['g','h','i']]
+buildBoard(board)
+
+row = int(input('Enter a row: '))
+col = int(input('Enter a column: '))
+board[row-1][col-1] = 'X'
+buildBoard(board)
