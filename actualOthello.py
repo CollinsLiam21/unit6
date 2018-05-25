b9f1020147a12a8a27a95d009c2a688302c57372
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-05-25 09:59:10 -0400

    Updated from Brython Server: 5/25/2018 9:59:08 AM

--- actualOthello.py
+++ actualOthello.py
@@ -22,35 +22,8 @@ def redrawAll():
         for c in range(0,8):
             Sprite(greenGrid,(A*r,A*c))
             
-'''def mouseClick(event):
-    if data['gameOver'] == False:
-        if (event.x < A and event.y < A) and board[0][0] == 0:
-            Sprite(whiteCircle, (A/2,A/2))
-            board[0][0] = 1
-        elif (150 < event.x < 350 and event.y < 150) and isEmpty(2) == True:
-            Sprite(x, (215,10))
-            data['square2'] = 'x'
-        elif (350 < event.x < 550 and event.y < 150) and isEmpty(3) == True:
-            Sprite(x, (400,10))
-            data['square3'] = 'x'
-        elif (event.x < 150 and 150 < event.y < 350) and isEmpty(4) == True:
-            Sprite(x, (25,180))
-            data['square4'] = 'x'
-        elif (150 < event.x < 350 and 150 < event.y < 350) and isEmpty(5) == True:
-            Sprite(x, (215,180))
-            data['square5'] = 'x'
-        elif (350 < event.x < 550 and 150 < event.y < 350) and isEmpty(6) == True:
-            Sprite(x, (400,180))
-            data['square6'] = 'x'
-        elif (event.x < 150 and 350 < event.y < 550) and isEmpty(7) == True:
-            Sprite(x, (25,380))
-            data['square7'] = 'x'
-        elif (150 < event.x < 350 and 350 < event.y < 550) and isEmpty(8) == True:
-            Sprite(x, (215,380))
-            data['square8'] = 'x'
-        elif (350 < event.x < 550 and 350 < event.y < 550) and isEmpty(9) == True:
-            Sprite(x, (400,380))
-            data['square9'] = 'x'''
+def mouseClick(event):
+    
 
 
 if __name__ == '__main__':
@@ -63,5 +36,5 @@ if __name__ == '__main__':
     redrawAll()
     
     
-    #App().listenMouseEvent('click',mouseClick)
+    App().listenMouseEvent('click',mouseClick)
     App().run()
