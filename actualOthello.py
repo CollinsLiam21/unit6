74bb9f4f9c6c39293153295fc894773247bccd71
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-05-24 20:43:38 -0400

    Updated from Brython Server: 5/24/2018 8:43:37 PM

--- actualOthello.py
+++ actualOthello.py
@@ -4,6 +4,8 @@
 
 from ggame import *
 
+A = 500
+
 def buildBoard(board):
     for r in range(0,8):
         for c in range(0,8):
@@ -20,12 +22,23 @@ buildBoard(board)'''
 
 if __name__ == '__main__':
     
-    white = Color(0x000000,1)
-    black = Color(0xFFFFFF,1)
-    green = Color(0x00FF00,1)
+    white = Color(0xFFFFFF,1)
+    black = Color(0x000000,1)
+    green = Color(0x008000,1)
     
-    greenGrid = RectangleAsset(500,500,LineStyle(1,black),green)
+    greenGrid = RectangleAsset(A,A,LineStyle(4,black),green)
+    blackLine = LineAsset(0,A,LineStyle(4,black)) #x_endpoint, y_endpoint, lineStyle
+    
+    
     Sprite(greenGrid)
+    Sprite(blackLine, (A/8,0))
+    Sprite(blackLine, ((A/8)*2,0))
+    Sprite(blackLine, ((A/8)*3,0))
+    Sprite(blackLine, ((A/8)*4,0))
+    Sprite(blackLine, ((A/8)*5,0))
+    Sprite(blackLine, ((A/8)*6,0))
+    Sprite(blackLine, ((A/8)*7,0))
+    
     
     #App().listenMouseEvent('click',mouseClick)
     App().run()
