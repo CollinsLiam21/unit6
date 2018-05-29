23b5336f53e8ec9e7416f64c7c5ae32bec4b7c22
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-05-29 08:06:41 -0400

    Updated from Brython Server: 5/29/2018 8:06:40 AM

--- actualOthello.py
+++ actualOthello.py
@@ -36,9 +36,9 @@ def mouseClick(event):
     if i%2 == 0:
         Sprite(whiteCircle, (column*A,row*A))
         i+=1
-    if i%2 != 0:
+    '''if i%2 != 0:
         Sprite(blackCircle, (column*A,row*A))
-        i+=1
+        i+=1'''
 
 
 if __name__ == '__main__':
