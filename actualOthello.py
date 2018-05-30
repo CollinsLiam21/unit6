5552387f324636d6d3fa3b935cb1c1fbda6bfcc6
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-05-30 08:38:35 -0400

    Updated from Brython Server: 5/30/2018 8:38:34 AM

--- actualOthello.py
+++ actualOthello.py
@@ -35,6 +35,7 @@ def flipEast(rowLast,colLast):
     while data['board'][rowLast][colLast + i] == 2:
         i += 1
     while data['board'][rowLast][colLast + i] == 2:
+        data['board'][rowLast][colLast + i] = 1
         i -= 1
     
     if data['board'][rowLast][colLast] == 1 and data['board'][rowLast][colLast+2] == 1 and data['board'][rowLast][colLast+1] == 2:
