41864aae489779474bba2a11c13135cdaf59d231
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-05-30 08:40:33 -0400

    Updated from Brython Server: 5/30/2018 8:40:33 AM

--- actualOthello.py
+++ actualOthello.py
@@ -39,10 +39,6 @@ def flipEast(rowLast,colLast):
         i -= 1
     redrawAll()
     
-    if data['board'][rowLast][colLast] == 1 and data['board'][rowLast][colLast+2] == 1 and data['board'][rowLast][colLast+1] == 2:
-        data['board'][rowLast][colLast+1] = 1
-        redrawAll()
-    
             
 
 def mouseClick(event):
